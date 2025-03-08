07f5e28adaffb45e604292b5fb1036637019159e
Pritam Mandal <pritammandal1717@gmail.com>
2025-03-08 22:50:37 +0530

    some changes are made

--- apps/user-app/components/AddMoneyCard.tsx
+++ apps/user-app/components/AddMoneyCard.tsx
@@ -17,7 +17,7 @@ const SUPPORTED_BANKS = [{
 }];
 
 export const AddMoney = () => {
-    // const [redirectUrl, setRedirectUrl] = useState(SUPPORTED_BANKS[0]?.redirectUrl);
+    const [redirectUrl, setRedirectUrl] = useState(SUPPORTED_BANKS[0]?.redirectUrl);
     const [amount, setAmount] = useState("");
     const [provider, setProvider] = useState(SUPPORTED_BANKS[0]?.name || "");
     return <Card title="Add Money">
@@ -29,7 +29,7 @@ export const AddMoney = () => {
             Bank
         </div>
         <Select onSelect={(value) => {
-            // setRedirectUrl(SUPPORTED_BANKS.find(x => x.name === value)?.redirectUrl || "")
+            setRedirectUrl(SUPPORTED_BANKS.find(x => x.name === value)?.redirectUrl || "")
             setProvider(SUPPORTED_BANKS.find(x => x.name === value)?.name || "")
         }} options={SUPPORTED_BANKS.map(x => ({
             key: x.id,
@@ -38,7 +38,7 @@ export const AddMoney = () => {
         <div className="flex justify-center pt-4">
             <Button onClick={async() => {
                 await onRampTransactions(Number(amount), provider)
-                // window.location.href = redirectUrl || "";
+                window.location.href = redirectUrl || "";
             }}>
             Add Money
             </Button>
